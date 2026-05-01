5f3eb08d16213b8d4b6a56cbaabf1cf670616e9c
roy.wang <roy.wang@goanimate.com>
2026-05-01 16:26:10 +0800

    Rename event bus to action queue

--- scenes/action_queue.gd
+++ scenes/action_queue.gd
@@ -1,10 +1,17 @@
 extends Node
 
+var _event_queue: Array[Variant] = []
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print('event bus ready');
+	print('action queue ready');
 	pass # Replace with function body.
+
+
+func enQueue(event: Variant) -> void:
+	_event_queue.append(event)
+	print(_event_queue);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/debugger/button_b.gd
+++ scenes/debugger/button_b.gd
@@ -1,9 +1,17 @@
 extends Button
 
+@onready var action_queue: Node = get_parent().get_parent()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pressed.connect(_on_pressed)
+
+
+func _on_pressed() -> void:
+	action_queue.enQueue({
+		"source": name,
+		"text": text,
+	})
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
