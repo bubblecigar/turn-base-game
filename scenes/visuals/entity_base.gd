838b4fe32b3bd29c3f072a01b2a09024cc00f55c
roy.wang <roy.wang@goanimate.com>
2026-05-09 11:36:26 +0800

    Remove attack entity flow

--- scenes/visuals/entity_base.gd
+++ scenes/visuals/entity_base.gd
@@ -5,12 +5,6 @@ class_name EntityBoardView
 const MOVE_ANIMATION_SECONDS := 1.75
 const MOVE_ANIMATION_NAME := &"entity_move"
 const WALK_STEP_SECONDS := 0.35
-const ATTACK_ANIMATION_SECONDS := 0.28
-const ATTACK_ANIMATION_NAME := &"entity_attack"
-const ATTACK_LUNGE_PIXELS := 28.0
-const HIT_ANIMATION_SECONDS := 0.32
-const HIT_ANIMATION_NAME := &"entity_hit"
-const HIT_SHAKE_PIXELS := 8.0
 const ID_LABEL_FONT_SIZE := 8.0
 const ID_LABEL_HEIGHT := 16.0
 
@@ -18,11 +12,7 @@ var _entity_id := &""
 var _entity: Dictionary = {}
 var _id_label: Label
 var _move_tween: Tween
-var _attack_tween: Tween
-var _hit_tween: Tween
 var _move_animation_id := &""
-var _attack_animation_id := &""
-var _hit_animation_id := &""
 var _is_moving := false
 var _move_time := 0.0
 
@@ -43,7 +33,6 @@ func _ready() -> void:
 	state_store.entities_updated.connect(_on_entities_updated)
 	state_store.board_init.connect(_on_board_init)
 	state_store.entity_moved.connect(_on_entity_moved)
-	state_store.entity_attack_pair_triggered.connect(_on_entity_attack_pair_triggered)
 	_refresh_from_state()
 
 
@@ -120,15 +109,6 @@ func _on_entity_moved(entity_id: StringName, _next_position: Vector2, _previous_
 	_start_move_animation()
 
 
-func _on_entity_attack_pair_triggered(attacker_id: StringName, target_id: StringName) -> void:
-	if _entity_id == attacker_id:
-		print("attacker visual received attack signal: ", attacker_id, " -> ", target_id)
-		_play_attack_animation(target_id)
-	elif _entity_id == target_id:
-		print("receiver visual received attack signal: ", attacker_id, " -> ", target_id)
-		_play_hit_animation(attacker_id)
-
-
 func _on_move_tween_finished(animation_id: StringName) -> void:
 	animation_tracker.consume_animation(animation_id)
 
@@ -138,25 +118,6 @@ func _on_move_tween_finished(animation_id: StringName) -> void:
 		_stop_move_animation()
 
 
-func _on_attack_tween_finished(animation_id: StringName) -> void:
-	animation_tracker.consume_animation(animation_id)
-
-	if _attack_animation_id == animation_id:
-		_attack_animation_id = &""
-		_attack_tween = null
-		_update_board_position()
-
-
-func _on_hit_tween_finished(animation_id: StringName) -> void:
-	animation_tracker.consume_animation(animation_id)
-
-	if _hit_animation_id == animation_id:
-		_hit_animation_id = &""
-		_hit_tween = null
-		modulate = Color.WHITE
-		_update_board_position()
-
-
 func _consume_active_move_animation() -> void:
 	if _move_animation_id == &"":
 		return
@@ -164,82 +125,6 @@ func _consume_active_move_animation() -> void:
 	animation_tracker.consume_animation(_move_animation_id)
 	_move_animation_id = &""
 	_stop_move_animation()
-
-
-func _consume_active_attack_animation() -> void:
-	if _attack_animation_id == &"":
-		return
-
-	animation_tracker.consume_animation(_attack_animation_id)
-	_attack_animation_id = &""
-
-
-func _consume_active_hit_animation() -> void:
-	if _hit_animation_id == &"":
-		return
-
-	animation_tracker.consume_animation(_hit_animation_id)
-	_hit_animation_id = &""
-	modulate = Color.WHITE
-
-
-func _play_attack_animation(target_id: StringName) -> void:
-	if animation_tracker == null:
-		return
-
-	var target_bottom_position := _get_entity_board_bottom_position(target_id)
-	var own_bottom_position := _get_entity_board_bottom_position(_entity_id)
-	if target_bottom_position == Vector2.INF or own_bottom_position == Vector2.INF:
-		return
-
-	if _attack_tween:
-		_attack_tween.kill()
-		_consume_active_attack_animation()
-
-	var start_position := position
-	var attack_offset := target_bottom_position - own_bottom_position
-	if attack_offset.length() > ATTACK_LUNGE_PIXELS:
-		attack_offset = attack_offset.normalized() * ATTACK_LUNGE_PIXELS
-
-	var animation_id: StringName = animation_tracker.register_animation(ATTACK_ANIMATION_NAME)
-	_attack_animation_id = animation_id
-	_attack_tween = create_tween()
-	_attack_tween.set_trans(Tween.TRANS_QUAD)
-	_attack_tween.set_ease(Tween.EASE_OUT)
-	_attack_tween.tween_property(self, "position", start_position + attack_offset, ATTACK_ANIMATION_SECONDS * 0.45)
-	_attack_tween.tween_property(self, "position", start_position, ATTACK_ANIMATION_SECONDS * 0.55)
-	_attack_tween.finished.connect(_on_attack_tween_finished.bind(animation_id))
-
-
-func _play_hit_animation(attacker_id: StringName) -> void:
-	if animation_tracker == null:
-		return
-
-	var attacker_bottom_position := _get_entity_board_bottom_position(attacker_id)
-	var own_bottom_position := _get_entity_board_bottom_position(_entity_id)
-	if attacker_bottom_position == Vector2.INF or own_bottom_position == Vector2.INF:
-		return
-
-	if _hit_tween:
-		_hit_tween.kill()
-		_consume_active_hit_animation()
-
-	var start_position := position
-	var hit_direction := own_bottom_position - attacker_bottom_position
-	if hit_direction == Vector2.ZERO:
-		hit_direction = Vector2.RIGHT
-
-	var shake_offset := hit_direction.normalized() * HIT_SHAKE_PIXELS
-	var animation_id: StringName = animation_tracker.register_animation(HIT_ANIMATION_NAME)
-	_hit_animation_id = animation_id
-	_hit_tween = create_tween()
-	_hit_tween.set_parallel(true)
-	_hit_tween.tween_property(self, "modulate", Color(1.0, 0.55, 0.55), HIT_ANIMATION_SECONDS * 0.5)
-	_hit_tween.tween_property(self, "position", start_position + shake_offset, HIT_ANIMATION_SECONDS * 0.25)
-	_hit_tween.chain().tween_property(self, "position", start_position - shake_offset * 0.55, HIT_ANIMATION_SECONDS * 0.25)
-	_hit_tween.chain().tween_property(self, "position", start_position, HIT_ANIMATION_SECONDS * 0.25)
-	_hit_tween.parallel().tween_property(self, "modulate", Color.WHITE, HIT_ANIMATION_SECONDS * 0.25)
-	_hit_tween.finished.connect(_on_hit_tween_finished.bind(animation_id))
 
 
 func _start_move_animation() -> void:
