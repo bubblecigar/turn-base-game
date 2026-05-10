1493723e939da46806846dca4df9bcbb97e96737
roy.wang <roy.wang@goanimate.com>
2026-05-10 17:36:49 +0800

    restrict random attacks to horizontal only

--- scenes/game_control/game_loop.gd
+++ scenes/game_control/game_loop.gd
@@ -12,6 +12,10 @@ const ACTION_CATEGORY_ORDER := [
 	ACTION_CATEGORY_MOVE,
 	ACTION_CATEGORY_ATTACK,
 ]
+const ATTACK_VECTORS := [
+	Vector2i.LEFT,
+	Vector2i.RIGHT,
+]
 const MOVE_VECTORS := [
 	Vector2i.LEFT,
 	Vector2i.RIGHT,
@@ -118,8 +122,17 @@ func _create_random_entity_action(entity_id: StringName, alive_entity_ids: Array
 
 
 func _create_random_attack_action(entity_id: StringName) -> Dictionary:
+	var board: Dictionary = state_store.get_value(&"board", {})
 	var own_cell := _get_entity_board_index(entity_id)
 	if own_cell == Vector2i(-1, -1):
+		return {}
+
+	var valid_vectors: Array[Vector2i] = []
+	for vector: Vector2i in ATTACK_VECTORS:
+		if _has_board_cell(board, own_cell + vector):
+			valid_vectors.append(vector)
+
+	if valid_vectors.is_empty():
 		return {}
 
 	return {
@@ -129,7 +142,7 @@ func _create_random_attack_action(entity_id: StringName) -> Dictionary:
 			"args": {
 				"damage": randi_range(DAMAGE_MIN, DAMAGE_MAX),
 				"source": "game_loop",
-				"vector": MOVE_VECTORS[randi_range(0, MOVE_VECTORS.size() - 1)],
+				"vector": valid_vectors[randi_range(0, valid_vectors.size() - 1)],
 			},
 		},
 	}
