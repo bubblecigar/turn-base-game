c7d9d9bbf58b3f043e168cd7f195d2f67cf34a20
roy.wang <roy.wang@goanimate.com>
2026-05-10 17:17:52 +0800

    move game_loop under game_control, rename stage_control to game_control

--- scenes/game_control/stage_controller.gd
+++ scenes/game_control/stage_controller.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const STAGES_PATH := "res://scenes/stage_control/stage_templates.json"
+const STAGES_PATH := "res://scenes/game_control/stage_templates.json"
 
 @export var stage_key: String = ""
 
