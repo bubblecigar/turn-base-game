90337416e2e93a04231fcac8b44d45b8fb95c249
roy.wang <roy.wang@goanimate.com>
2026-05-13 12:54:14 +0800

    add selection indicator to entity

--- scenes/visuals/entity_base.gd
+++ scenes/visuals/entity_base.gd
@@ -69,6 +69,8 @@ const PROJECTILE_ATTACK_SECONDS := 0.55
 const PROJECTILE_ARC_CELL_HEIGHT := 3.0
 const PROJECTILE_ROCK_COLOR := Color(0.35, 0.32, 0.28, 1.0)
 const PROJECTILE_ROCK_BOTTOM_OFFSET := 11.0
+const SELECTION_RING_COLOR := Color(1.0, 0.9, 0.2, 1.0)
+const SELECTION_RING_THICKNESS := 2.0
 
 var _entity_id := &""
 var _entity: Dictionary = {}
@@ -86,6 +88,7 @@ var _id_label: Label
 var _damage_label: Label
 var _cast_result_label: Label
 var _cast_type_label: Label
+var _selection_ring: ReferenceRect
 var _move_tween: Tween
 var _hit_tween: Tween
 var _damage_tween: Tween
@@ -136,6 +139,7 @@ func _ready() -> void:
 	state_store.entity_moved.connect(_on_entity_moved)
 	state_store.entity_focus_changed.connect(_on_entity_focus_changed)
 	state_store.cast_resolved.connect(_on_cast_resolved)
+	state_store.entity_selection_changed.connect(_on_entity_selection_changed)
 	action_handler.cast_performed.connect(_on_cast_performed)
 	action_handler.attack_performed.connect(_on_attack_performed)
 	action_handler.attack_prepared.connect(_on_attack_prepared)
@@ -424,6 +428,10 @@ func _on_cast_resolved(caster_id: StringName, result: bool) -> void:
 		return
 
 	_finish_cast_visual(result)
+
+
+func _on_entity_selection_changed(_entity_id_selected: StringName, _previous: StringName) -> void:
+	_update_selection_indicator()
 
 
 func _on_move_tween_finished(animation_id: StringName) -> void:
@@ -797,6 +805,7 @@ func _set_entity(entity_state: Dictionary) -> void:
 	_update_id_label()
 	_update_hp_bar()
 	_update_focus_label()
+	_update_selection_indicator()
 
 
 func _get_previous_entity(previous_entities: Variant) -> Dictionary:
@@ -1087,9 +1096,27 @@ func _create_hp_bar() -> void:
 	_focus_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 	add_child(_focus_label)
 
+	_selection_ring = ReferenceRect.new()
+	_selection_ring.border_color = SELECTION_RING_COLOR
+	_selection_ring.border_width = SELECTION_RING_THICKNESS
+	_selection_ring.editor_only = false
+	_selection_ring.visible = false
+	add_child(_selection_ring)
+
 
 func _update_id_label() -> void:
 	pass
+
+
+func _update_selection_indicator() -> void:
+	if _selection_ring == null:
+		return
+	var is_selected: bool = state_store != null and state_store.get_selected_entity_id() == _entity_id
+	_selection_ring.visible = is_selected
+	if is_selected:
+		var visual_size := get_visual_size()
+		_selection_ring.position = Vector2.ZERO
+		_selection_ring.size = visual_size
 
 
 func _update_hp_bar() -> void:
